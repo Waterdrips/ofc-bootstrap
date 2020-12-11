01d4ec67c8afd05ac50c58937e84564eb55029c3
Alistair Hey <alistair@heyal.co.uk>
2020-12-11 21:57:45 +0000

    Verify ci command on make
    Signed-off-by: Alistair Hey <alistair@heyal.co.uk>

--- hack/verify-ci.sh
+++ hack/verify-ci.sh
@@ -1,9 +1,23 @@
-#! /bin/sh
+#! /bin/bash
 
-set -euf -o pipefail
+set -euf
 
 FILE=/tmp/helm_list.json
 
+# Check the secrets are created (ones in example_init.yaml)
+
+# Check ofc functions are installed, ones from stack.yaml
+
+# Check github functions are installed
+
+# Check core components are installed (edge-auth etc)
+
+# Check the gateway is up (using a port forward)
+
+# Check the dashboard is up
+
+
+# Check helm thinks the components are installed and deployed
 helm ls -A -o json > $FILE
 cat $FILE | jq '.[] | select(.name=="cert-manager").status' | xargs -I % test % = "deployed"
 cat $FILE | jq '.[] | select(.name=="cloud-minio").status' | xargs -I % test % = "deployed"
